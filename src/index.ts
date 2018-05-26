53c85330cb0b4e3c472253508391070fa085a881
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-05-25 18:35:35 -0700

    work

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,80 @@
 import {Command, flags} from '@oclif/command'
+import ux from 'cli-ux'
+import * as _ from 'lodash'
+
+const packages = require('all-the-package-names')
+const listNpmContents = require('list-npm-contents')
+
+const exclude = [
+  '.babelrc',
+  '.bithoundrc',
+  '.bower.json',
+  '.bowerrc',
+  '.circleci',
+  '.coveralls.yml',
+  '.dockerignore',
+  '.editorconfig',
+  '.eslintignore',
+  '.eslintrc',
+  '.eslintrc.js',
+  '.eslintrc.json',
+  '.eslint.json',
+  '.flowconfig',
+  '.git-hooks',
+  '.gitattributes',
+  '.github',
+  '.gitignore',
+  '.gitmodules',
+  '.idea',
+  '.jsbeautifyrc',
+  '.jscrc',
+  '.jscsrc',
+  '.jsdoc.json',
+  '.jshintignore',
+  '.jshintrc',
+  '.lint',
+  '.lintignore',
+  '.npmignore',
+  '.nvmrc',
+  '.postcssrc.js',
+  '.prettierignore',
+  '.prettierrc',
+  '.slugignore',
+  '.sublime-project',
+  '.tern-project',
+  '.testignore',
+  '.travis.yml',
+  '.istanbul.yml',
+  '.vimrc',
+]
 
 class FindNpmDotfiles extends Command {
-  static description = 'describe the command here'
-
   static flags = {
-    // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
   }
 
   static args = [{name: 'file'}]
 
   async run() {
-    const {args, flags} = this.parse(FindNpmDotfiles)
-
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    this.parse(FindNpmDotfiles)
+    let name: string
+    while (name = _.sample(packages)) {
+      ux.action.start(`fetching ${name}`)
+      try {
+        let files = await listNpmContents(name) as string[]
+        files = files.filter(f => {
+          if (!f.startsWith('.')) return false
+          if (exclude.find(e => f === e || f.startsWith(e + '/'))) return false
+          return true
+        })
+        for (let f of files) {
+          ux.log(`${name}: ${f}`)
+        }
+      } catch (err) {
+        ux.warn(name)
+        ux.warn(err)
+      }
     }
   }
 }
